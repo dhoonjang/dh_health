c0d90fc4278d207bdcfafe90ab2d5acd8403ea80
dhoonjang <dhoonjang@gmail.com>
2021-01-24 23:58:59 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>동훈헬스</h1>
+      <div className="first">
+        <h2>새해에도 코로나 핑계대실 건가요?</h2>
+        <h3>
+          자기 자신을 아끼고 싶다면 내일로 미루지말고 지금 바로 시작하세요!
+        </h3>
+      </div>
+      <div className="second">
+        <h2>마스크 방해없는 무산소 운동!</h2>
+        <h3>
+          무산소 운동은 유산소 운동보다 마스크의 방해를 덜 받으며 운동을 할 수
+          있습니다!
+        </h3>
+      </div>
+      <div className="third">
+        <h2>여유로운 공간, 넉넉한 기구</h2>
+        <h3>
+          여유로운 공간, 좋은 기구들 그리고 철저한 방역으로 회원님을 맞이할
+          준비를 하고 있습니다!
+        </h3>
+      </div>
+      <div className="fourth">
+        <h2>밀접한 PT x, 정확한 PT o</h2>
+        <h3>
+          동훈헬스의 전문 PT 선생님들은 회원님을 세심히 관찰하며 정확한 피드백을
+          합니다!
+        </h3>
+      </div>
     </div>
   );
 }
