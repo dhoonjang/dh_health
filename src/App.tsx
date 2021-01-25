6e6a9a96f07095654fb7db60e68a251848cf0179
dhoonjang <dhoonjang@gmail.com>
2021-01-25 13:56:11 +0900

    feat: content

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="title">동훈헬스</div>
+      <div className="title">
+        동훈헬스
+        <div className="phone">02) 1234-1234</div>
+      </div>
       <div className="first">
         <h2>
           새해에도 코로나
           <br />
           핑계대실 건가요?
         </h2>
-        <h3>
-          자기 자신을 아끼고 싶다면 내일로 미루지말고 지금 바로 시작하세요!
-        </h3>
+        <h3>내일로 미루지말고 지금 바로 시작하세요!</h3>
+        <div className="content">
+          비포 에프터 사진으로 다이어트 자극
+          <br />
+          <br />* 헬스장에서 운영되는 다이어트, 자기관리 프로그램 소개
+        </div>
       </div>
       <div className="second">
         <h2>
@@ -25,6 +31,12 @@ function App() {
           무산소 운동은 유산소 운동보다 마스크의 방해를 덜 받으며 운동을 할 수
           있습니다!
         </h3>
+        <div className="content">
+          안정된 호흡으로 고중량을 드는 운동 영상으로 운동 자극
+          <br />
+          <br />* 무산소 운동과 유산소 운동의 전문적인 의학용어를 활용한 차이점
+          설명
+        </div>
       </div>
       <div className="third">
         <h2>
@@ -36,6 +48,12 @@ function App() {
           여유로운 공간, 좋은 기구들 그리고 철저한 방역으로 회원님을 맞이할
           준비를 하고 있습니다!
         </h3>
+        <div className="content">
+          헬스장 공간 사진(샤워실, 라커룸, 운동기구 등) 들 슬라이드 뷰
+          <br />
+          <br />* Cybex, Hammer Strength, Star Trac, Life Fitness, Techno Gym
+          등의 전문적인 머신 및 시설 용어 활용
+        </div>
       </div>
       <div className="fourth">
         <h2>
@@ -44,9 +62,22 @@ function App() {
           정확한 PT o
         </h2>
         <h3>
-          동훈헬스의 전문 PT 선생님들은 회원님을 세심히 관찰하며 정확한 피드백을
+          동훈헬스의 전문 PT 선생님들이 회원님을 세심히 관찰하며 정확한 피드백을
           합니다!
         </h3>
+        <div className="content">
+          PT에서 알려주게 되는 구체적인 피드백 예시(자주 실수하는 자세, 자극을
+          받아야하는 부위 등) 들 슬라이드 뷰
+        </div>
+      </div>
+      <div className="last">
+        <h2>
+          동훈헬스
+          <br />
+          자신을 아끼는 가장 정직한 방법
+        </h2>
+        <h3>02) 1234-1234</h3>
+        <div className="content">헬스장 장소를 나타내는 지도 및 운영시간</div>
       </div>
     </div>
   );
